21ee496277db90266af7537675c445aa099a38a2
paarth agarwal <agarwal.paarth93@gmail.com>
2020-11-25 11:05:21 +0530

    .net core 3.1

--- AngularApp-ECU/Pages/Error.cshtml.cs
+++ AngularApp-ECU/Pages/Error.cshtml.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AngularApp_ECU.Pages
+namespace MedicineTracker.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
